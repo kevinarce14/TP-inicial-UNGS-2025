619e053a69316c554d3596ed4aae2b04d396ac9c
Kevin Arce <kevindamian1702@gmail.com>
2025-09-20 17:23:02 -0300

    railway

--- web/app.py
+++ web/app.py
@@ -343,4 +343,5 @@ async def ejecutar_totem(modo: str = Form(default="entry")):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
